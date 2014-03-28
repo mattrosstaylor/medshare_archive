1acee32edb109479cac9ccc305fe21fff4fa33d5
Matt R Taylor <matt.ross.taylor@gmail.com>
2014-03-28 18:05:57 +0000

    renamed stylesheet to conform to xyz ordering. Removed an error from eprint_fields_automatic. Updated the workflow to fit with the new Permissions configuration format.

--- cfg/cfg.d/z_medshare_eprint_fields_automatic.pl
+++ cfg/cfg.d/z_medshare_eprint_fields_automatic.pl
@@ -8,9 +8,9 @@ $c->{set_eprint_automatic_fields} = sub
 
 	$repo->call('medshare_archive_set_eprint_automatic_fields', $eprint);
 
-	my $programme_year = $eprint->value( "course_programme_year");
+	my $programme_year = $eprint->value( "course_programme_year" );
 	# check for special case programme/year/module selection
-	if ( $programme_year eq 'bm4_y1' or $programme_year eq 'bm4_y2' or $programme_year eq 'pg')
+	if ( defined $programme_year and ( $programme_year eq 'bm4_y1' or $programme_year eq 'bm4_y2' or $programme_year eq 'pg' ) )
 	{
 		$eprint->set_value( 'course_module', 'unspecified' );
 	} 
